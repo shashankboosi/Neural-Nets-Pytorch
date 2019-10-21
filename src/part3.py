19764e8d5fb27946e2f10ef2df1b6cc852eebd7e
shashankboosi <shashank1872@gmail.com>
2019-10-22 09:14:16 +1100

    [Pytorch] Add the feed forward neural net

--- src/part3.py
+++ src/part3.py
@@ -43,6 +43,23 @@ class FeedForward(nn.Module):
     TODO: Implement the following network structure
     Linear (256) -> ReLU -> Linear(64) -> ReLU -> Linear(10) -> ReLU-> LogSoftmax
     """
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(784, 256)
+        self.fc2 = nn.Linear(256, 64)
+        self.fc3 = nn.Linear(64, 10)
+
+    def forward(self, x):
+        x = x.view(x.shape[0], -1)
+
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = F.log_softmax(x, dim=1)
+
+        return x
+
+
 
 
 class CNN(nn.Module):
@@ -57,6 +74,13 @@ class CNN(nn.Module):
     Hint: You will need to reshape outputs from the last conv layer prior to feeding them into
     the linear layers.
     """
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(784, 256)
+
+    def forward(self, x):
+        x = x.view(x.shape[0], -1)
+
 
 class NNModel:
     def __init__(self, network, learning_rate):
