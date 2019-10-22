1f2eadecaf36195e39e884297337aeac5b201da3
shashankboosi <shashank1872@gmail.com>
2019-10-22 13:03:37 +1100

    Add the conv net network for the KMNIST dataset

--- src/part3.py
+++ src/part3.py
@@ -43,6 +43,7 @@ class FeedForward(nn.Module):
     TODO: Implement the following network structure
     Linear (256) -> ReLU -> Linear(64) -> ReLU -> Linear(10) -> ReLU-> LogSoftmax
     """
+
     def __init__(self):
         super().__init__()
         self.fc1 = nn.Linear(784, 256)
@@ -60,8 +61,6 @@ class FeedForward(nn.Module):
         return x
 
 
-
-
 class CNN(nn.Module):
     """
     TODO: Implement CNN Network structure
@@ -74,12 +73,25 @@ class CNN(nn.Module):
     Hint: You will need to reshape outputs from the last conv layer prior to feeding them into
     the linear layers.
     """
+
     def __init__(self):
         super().__init__()
-        self.fc1 = nn.Linear(784, 256)
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=10, kernel_size=5, stride=1)
+        self.conv2 = nn.Conv2d(in_channels=10, out_channels=50, kernel_size=5, stride=1)
+        print(self.conv1)
+        self.fc1 = nn.Linear(50 * 4 * 4, 256)
+        self.fc2 = nn.Linear(256, 10)
 
     def forward(self, x):
         x = x.view(x.shape[0], -1)
+
+        x = F.max_pool2d(F.relu(self.conv1(x)), 2)
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = x.reshape(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = F.log_softmax(self.fc2(x), dim=1)
+
+        return x
 
 
 class NNModel:
