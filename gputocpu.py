e504b3900a458e83d1a149d5011800da3a3b5c36
shashankboosi <shashank1872@gmail.com>
2019-11-26 16:35:09 +1100

    [GPU] Code to change the model.pth from GPU to CPU

---
+++ gputocpu.py
@@ -0,0 +1,10 @@
+# convert.py
+import torch
+from part3 import Network, PreProcessing
+
+device = torch.device('cpu')
+net = Network().to(device)
+print(f'Loading model..')
+net.load_state_dict(torch.load('model.pth', map_location=torch.device(device)))
+print(f'Saving model as {device}.')
+torch.save(net.state_dict(), './model.pth')
